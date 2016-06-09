9d0beb4f9d3b69adbb48069498ee7784bbff7f08
aldomellado <aldomellado@udec.cl>
2016-06-09 15:50:38 -0400

    Reconoce bien

--- Lab3/algorithms.py
+++ Lab3/algorithms.py
@@ -8,88 +8,87 @@ import numpy as np
 #import utils
 import cv2
 
-##c = Camera()                            # Iniciamos la camara
-##time.sleep(2)                           
-##img=c.getImage()                       
-##img.save('image.png')
+print 'Capturando Imagen'
+
+c = Camera()                            # Iniciamos la camara
+time.sleep(2)                           
+img=c.getImage()                       
+img.save('image.png')
+
+print 'Imagen capturada'
 
 #lectura de la imagen obtenida para su procesamiento
 
-img=Image("lunares.png")
+print 'Procesando'
+
+
+img=Image("image.png")
 Mask=Image("mask.png")				#Creamos la máscara.
-(red, green, blue)=img.splitChannels()
+(red, green, blue)=img.splitChannels(False)
 red.save("Redimage.png")
 blue.save("Blueimage.png")
 green.save("Greenimage.png")
 imgGris=img.grayscale()
 imgGris.save("Grayimage.png")
-
-#Procedemos a testear Canny
-                                    
-EdgesRED  =red.edges(t1=30,t2=80)                   # Borde RGB
-EdgesGREEN=green.edges(t1=30,t2=80)
-EdgesBLUE =blue.edges(t1=30,t2=80)
-ERed  =  EdgesRED*Mask + img
-EBlue  = EdgesBLUE*Mask + img
-EGreen = EdgesGREEN*Mask + img
-ERed.save("ERed.png")
-ERed.show()
-EGreen.save("EGreen.png")
-EGreen.show()
-EBlue.save("EBlue.png")
-EBlue.show()
-                                                    #Borde GrayScale
-EdgesGray = imgGray.edges(t1=30,t2=80)
-EGray = EdgesGray*mask + img	
-EdgesGray.save("EGray.png")
-EGray.show()
+Mask=Mask.resize(320,240)
 
 #Procedemos a testear Blob
 
+print 'Blob'
+
 blob=green.findBlobs()                  
 blob.draw((200,0,0),width=3)
-img.addDrawingLayer(Greenimage.dl())
+img.addDrawingLayer(green.dl())
 img.save("Blob_Green.png")
 blob=red.findBlobs()                  
 blob.draw((200,0,0),width=3)
-img.addDrawingLayer(Redimage.dl())
+img.addDrawingLayer(red.dl())
 img.save("Blob_red.png")
 blob=blue.findBlobs()                  
 blob.draw((200,0,0),width=3)
-img.addDrawingLayer(Blueimage.dl())
+img.addDrawingLayer(blue.dl())
 img.save("Blob_blue.png")
 blob=imgGris.findBlobs()                  
 blob.draw((200,0,0),width=3)
-img.addDrawingLayer(Grayimage.dl())
+img.addDrawingLayer(imgGris.dl())
 img.save("Blob_gris.png")
+img.show()
 
  #Distancia Color
-dist = red.colorDistance()
-bin = dist.binarize(70).morphClose()
-lines = bin.findLines(threshold=10,minlinelenght=15)
+print 'Color Distance'
+
+dist=red.colorDistance()
+bin=dist.binarize(75).morphClose()
+lines=bin.findLines(threshold=10,minlinelength=10)
 lines.draw(width=3)
 img.addDrawingLayer(bin.dl())
 img.save("Distance_Red.png")
-                            
-dist = gray.colorDistance()
-bin = dist.binarize(70).morphClose()
-lines = bin.findLines(threshold=10,minlinelenght=15)
+plt.title('Color Distance Red')
+img.show()
+
+dist = imgGris.colorDistance()
+bin=dist.binarize(75).morphClose()
+lines=bin.findLines(threshold=10,minlinelength=10)
 lines.draw(width=3)
 img.addDrawingLayer(bin.dl())
+plt.title('Color Distance Gray')
 img.save("Distance_Gray.png")
-                            
+img.show()
+
 dist = blue.colorDistance()
-bin = dist.binarize(70).morphClose()
-lines = bin.findLines(threshold=10,minlinelenght=15)
+bin=dist.binarize(75).morphClose()
+lines=bin.findLines(threshold=10,minlinelength=10)
 lines.draw(width=3)
 img.addDrawingLayer(bin.dl())
 img.save("Distance_Blue.png")
-                            
+plt.title('Color Distance Blue')
+img.show()
+
 dist = green.colorDistance()
-bin = dist.binarize(70).morphClose()
-lines = bin.findLines(threshold=10,minlinelenght=15)
+bin=dist.binarize(70).morphClose()
+lines=bin.findLines(threshold=10,minlinelength=15)
 lines.draw(width=3)
 img.addDrawingLayer(bin.dl())
 img.save("Distance_Green.png")
-
-                          
+plt.title('Color Distance Green')
+img.show()                          
